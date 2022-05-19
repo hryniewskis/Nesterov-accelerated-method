2a4761da6b453cdb5010b51ca0933052de1d2934
Szymon Hryniewski <80833393+hryniewskis@users.noreply.github.com>
2022-05-20 00:24:13 +0200

    adding optimizers.py and tests.ipnyb files

---
+++ optimizers.py
@@ -0,0 +1,107 @@
+import numpy as np
+from numpy.linalg import norm
+
+
+class Nesterov_Optimizers:
+    def __init__(self, gamma_u: float = 2, gamma_d: float = 2, lambda_: float = 1.0, max_iter: int = 1000):
+        assert (gamma_u > 1), "parameter gamma_u has to be greater than 1"
+        assert (gamma_d >= 1), "parameter gamma_d has to be greater or equal 1"
+        assert (lambda_ >= 0), "parameter lambda_ has to be nonnegative"
+        assert (max_iter > 0), "parameter max_iter has to be a positive integer"
+        
+        self.lambda_ = lambda_
+        self.gamma_u = gamma_u
+        self.gamma_d = gamma_d
+        self.max_iter = max_iter
+        self.is_trained = False
+        self.A = None
+        self.b = None
+        self.L = None
+        self.coef_ = None
+
+    def __function_f(self, x):
+        return 0.5 * norm(self.b - np.dot(self.A, x)) ** 2
+
+    def __gradient_f(self, x):
+        return self.A.T.dot(self.A).dot(x) - self.A.T.dot(self.b)
+
+    def __function_Psi(self, x):
+        return self.lambda_ * norm(x, ord=1)
+
+    def __objective(self, x):
+        return self.__function_f(x) + self.__function_Psi(x)
+
+    def __m_L(self, y, x, L):
+        # assert y.shape[0] == x.shape[0], "y and x have to be in the same shape"
+        return (
+                self.__function_f(y) + np.dot(self.__gradient_f(y), x - y) +
+                (L / 2) * norm(x - y) ** 2 + self.__function_Psi(x)
+               )
+
+    @staticmethod
+    def __minimum(a, b, d):
+        # TODO sprawdzanie a>0 i d>0
+        assert np.all(a > 0), "all elements of vector a have to be positive"
+        assert np.all(d >= 0), "all elements of vector d have to be nonnegative"
+        
+        return np.where(
+            b < -d,
+            -(b + d) / (2 * a),
+            np.where(np.logical_and(b > -1 * d, b < d), 0, -(b - d) / (2 * a)),
+        )
+
+    def __T_L(self, y, L):
+        # assert L > 0, "L has to be greater than 0"
+
+        a = np.array([L / 2 for i in range(y.shape[0])])
+        b = self.__gradient_f(y) - L * y
+        d = np.array([self.lambda_ for i in range(y.shape[0])])
+        return self.__minimum(a = a, b = b, d = d)
+
+    def __gradient_iteration(self, x, M):
+        L = M
+        T = self.__T_L(x, L)
+        while self.__objective(T) > self.__m_L(x, T, L):
+            L = L * self.gamma_u
+            T = self.__T_L(x, L)
+        S_L = norm(self.__gradient_f(T) - self.__gradient_f(x)) / norm(T - x)
+        return T, L, S_L
+
+    def __gradient_method(self, y, L):
+        L_k = L
+        stop_criterion = False
+        for iter in range(self.max_iter):
+            y, M = self.__gradient_iteration(y, L_k)[0:2]
+            L_k = max(L, M * 1.0 / self.gamma_d)
+            # TODO warunek stopu
+            if stop_criterion:
+                break
+            # print(self.objective(y))
+        self.is_trained = True
+        return y
+
+    def __dual_gradient_method(self, v, L):
+        print("not implemented")
+        pass
+
+    def __accelerated_method(v, L):
+        print("not implemented")
+        pass
+
+    def fit(self, X: np.matrix, y: np.array, method: str = "gradient_method"):
+        # sprawdzić czy liczba wierszy w X=długośc wektora y
+        # sprawdzić czy X nie jest macierzą zerową
+        self.A = X
+        self.coef_ = np.full(shape = X.shape[1], fill_value = 0)
+        self.b = y
+        self.L = norm(X) ** 2
+
+        if method == "gradient_method":
+            return self.__gradient_method(y = self.coef_, L = self.L)
+        elif method == "dual_gradient_method":
+            return self.__dual_gradient_method(v = self.coef_, L = self.L)
+        elif method == "accelerated_method":
+            return self.__accelerated_method(v = self.coef_, L = self.L)
+        else:
+            print("wrong argument")
+            return None
